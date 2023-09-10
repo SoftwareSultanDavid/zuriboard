8c26f1ff7051d32e57f66ede9a2c805782d765cb
David Igbayilola <43897620+SoftwareSultanDavid@users.noreply.github.com>
2023-09-10 17:04:09 +0100

    changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const { getCurrentUTCWithinRange, getCurrentDay } = require('./date');
-const port = 3000;
 
 app.get('/api', (req, res) => {
     const slack_name = req.query.slack_name;
@@ -29,7 +28,4 @@ app.get('/api', (req, res) => {
 })
 
 
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+module.exports = app;
